4b2c1c08b88dcc963fb4319d9e5109d7a72c70f2
Alex-rv150 <arangelvizcaino@gmail.com>
2024-10-14 17:39:10 -0500

    inicializar

--- src/main/java/core/RockRollRadio/RockRollRadio.java
+++ src/main/java/core/RockRollRadio/RockRollRadio.java
@@ -5,6 +5,7 @@ import core.Cancion.Cancion;
 import core.Persona.Artista;
 import core.Persona.Invitado;
 import core.Persona.Locutor;
+import core.Programa.Emision.Emision;
 import core.Programa.Programa;
 import java.util.ArrayList;
 
@@ -16,6 +17,7 @@ public class RockRollRadio {
     private ArrayList<Invitado> invitados;
     private ArrayList<Locutor> locutores;
     private ArrayList<Programa> programas;
+    private ArrayList<Emision> emisiones;
     
     
     
@@ -25,7 +27,65 @@ public class RockRollRadio {
     invitados = new ArrayList<>();
     locutores =new ArrayList<>();
     programas = new ArrayList<>();
+    emisiones = new ArrayList<>();
     }
+    
+    public boolean addLocutor (Locutor locutor){
+    if (!locutores.contains(locutor)){
+    locutores.add(locutor);
+    return true;
+    }else{
+    return false;
+    }
+    }
+    
+    
+    
+    public boolean addPrograma (Programa programa){
+            if (!programas.contains(programa)){
+                programas.add(programa);
+                return true;
+            }else{
+                return false;
+            }
+        }
+    
+    public boolean  addArtista (Artista artista){
+            if (!artistas.contains(artista)){
+                artistas.add(artista);
+                return true;
+            }else{
+                return false;
+            }
+        }
+            
+    
+    
+        public boolean  addCancion (Cancion cancion){
+            if (!canciones.contains(cancion)){
+                canciones.add(cancion);
+                return true;
+            }else{
+                return false;
+            }
+        }
+            
+        public boolean  addEmision (Emision  emision ){
+            if (!canciones.contains(emision)){
+                canciones.add(emision);
+                return true;
+            }else{
+                return false;
+            }
+        }
+            
+           
+                    
+                    
+    addInvitado
+    
+    
+    
     
    
     
