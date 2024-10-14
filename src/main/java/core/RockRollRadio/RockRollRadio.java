bb7de5d9da4c4871fc4c0862c0bddba7ed43b4d0
Alex-rv150 <arangelvizcaino@gmail.com>
2024-10-14 17:54:05 -0500

    inicializar

--- src/main/java/core/RockRollRadio/RockRollRadio.java
+++ src/main/java/core/RockRollRadio/RockRollRadio.java
@@ -18,113 +18,95 @@ public class RockRollRadio {
     private ArrayList<Locutor> locutores;
     private ArrayList<Programa> programas;
     private ArrayList<Emision> emisiones;
-    
-    
-    
-    public RockRollRadio (){
-    artistas = new ArrayList<>();
-    canciones = new ArrayList<>();
-    invitados = new ArrayList<>();
-    locutores =new ArrayList<>();
-    programas = new ArrayList<>();
-    emisiones = new ArrayList<>();
-    }
-    
-    public boolean addLocutor (Locutor locutor){
-    if (!this.locutores.contains(locutor)){
-    this.locutores.add(locutor);
-    return true;
-    }else{
-    return false;
-    }
-    }
-    
-    
-    
-    public boolean addPrograma (Programa programa){
-            if (!this.programas.contains(programa)){
-                this.programas.add(programa);
-                return true;
-            }else{
-                return false;
-            }
-        }
-    
-    public boolean  addArtista (Artista artista){
-            if (!this.artistas.contains(artista)){
-                this.artistas.add(artista);
-                return true;
-            }else{
-                return false;
-            }
-        }
-            
-    
-    
-        public boolean  addCancion (Cancion cancion){
-            if (!this.canciones.contains(cancion)){
-                this.canciones.add(cancion);
-                return true;
-            }else{
-                return false;
-            }
-        }
-            
-        public boolean  addEmision (Emision  emision ){
-            if (!this.emisiones.contains(emision)){
-                this.emisiones.add(emision);
-                return true;
-            }else{
-                return false;
-            }
-        }
-        
-        
-        public boolean  addInvitado (Invitado  invitado){
-            if (!this.invitados.contains(invitado)){
-                this.invitados.add(invitado);
-                return true;
-            }else{
-                return false;
-            }
-        }
-        
-        
-/* getts */
-        
-      public Locutor  getLocutor(int  index){
-                return this.locutores.get(index);
-        }   
-   
-  
-    
-    
-    
- public Programa  getPrograma(int  index){
-                return this.programas.get(index);
-        }
-    
- public Artista  getArtista (int  index){
-                return this.artistas.get(index);
-        }
-            
-    
-    
 
-            
-    public Cancion  getCancion (int  index){
-                return this.canciones.get(index);
+    public RockRollRadio() {
+        artistas = new ArrayList<>();
+        canciones = new ArrayList<>();
+        invitados = new ArrayList<>();
+        locutores = new ArrayList<>();
+        programas = new ArrayList<>();
+        emisiones = new ArrayList<>();
+    }
+
+    public boolean addLocutor(Locutor locutor) {
+        if (!this.locutores.contains(locutor)) {
+            this.locutores.add(locutor);
+            return true;
+        } else {
+            return false;
         }
-        
-          public Emision  getEmision (int  index){
-                return this.emisiones.get(index);
+    }
+
+    public boolean addPrograma(Programa programa) {
+        if (!this.programas.contains(programa)) {
+            this.programas.add(programa);
+            return true;
+        } else {
+            return false;
         }
-        
-        
-        public Invitado  getInvitado (int  index){
-                return this.invitados.get(index);
+    }
+
+    public boolean addArtista(Artista artista) {
+        if (!this.artistas.contains(artista)) {
+            this.artistas.add(artista);
+            return true;
+        } else {
+            return false;
         }
-            
+    }
+
+    public boolean addCancion(Cancion cancion) {
+        if (!this.canciones.contains(cancion)) {
+            this.canciones.add(cancion);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean addEmision(Emision emision) {
+        if (!this.emisiones.contains(emision)) {
+            this.emisiones.add(emision);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean addInvitado(Invitado invitado) {
+        if (!this.invitados.contains(invitado)) {
+            this.invitados.add(invitado);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /* getts */
+    public Locutor getLocutor(int index) {
+        return this.locutores.get(index);
+    }
+
+    public Programa getPrograma(int index) {
+        return this.programas.get(index);
+    }
+
+    public Artista getArtista(int index) {
+        return this.artistas.get(index);
+    }
+
+    public Cancion getCancion(int index) {
+        return this.canciones.get(index);
+    }
+
+    public Emision getEmision(int index) {
+        return this.emisiones.get(index);
+    }
+
+    public Invitado getInvitado(int index) {
+        return this.invitados.get(index);
+    }
+
            
                     
                 
