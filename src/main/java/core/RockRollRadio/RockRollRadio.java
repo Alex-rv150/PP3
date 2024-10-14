508243921d08379229467e1e8af4cf51f5837148
Alex-rv150 <arangelvizcaino@gmail.com>
2024-10-14 17:24:17 -0500

    inicializar

--- src/main/java/core/RockRollRadio/RockRollRadio.java
+++ src/main/java/core/RockRollRadio/RockRollRadio.java
@@ -3,11 +3,35 @@ package core.RockRollRadio;
 
 import core.Cancion.Cancion;
 import core.Persona.Artista;
+import core.Persona.Invitado;
+import core.Persona.Locutor;
+import core.Programa.Programa;
 import java.util.ArrayList;
 
 
 public class RockRollRadio {
     
     private ArrayList<Artista> artistas;
-    private ArrayList<Cancion> cancion;
+    private ArrayList<Cancion> canciones;
+    private ArrayList<Invitado> invitados;
+    private ArrayList<Locutor> locutores;
+    private ArrayList<Programa> programas;
+    
+    
+    
+    public RockRollRadio (){
+    artistas = new ArrayList<>();
+    canciones = new ArrayList<>();;
+    invitados = new ArrayList<>();;
+    locutores =new ArrayList<>();;
+    programas = new ArrayList<>();;
+    }
+    
+   
+    
+    
+  
+    
+    
+    
 }
